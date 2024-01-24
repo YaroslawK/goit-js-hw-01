8e42c88d042a623505e4f95ac79b79ae2ce38871
YaroslawK <xxjokerxx933@gmail.com>
2024-01-24 21:23:39 +0200

    fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${ country } will cost ${ totalPrice } credits`;
+    return `Shipping to ${ country } will cost ${ price + deliveryFee } credits`;
 }
 
 
